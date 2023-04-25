94781b1add767210fd92d6339c374278c9cffe1a
malcolm.dane.chellquist@gmail.com <malcolm.dane.chellquist@gmail.com>
2023-04-25 06:55:11 -0400

    opps. Added JSON fetch api using raw nytimes rss

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+
+$CONFIG = array(
+    // Example PostgreSQL configuration
+
+
+    // Example MySQL configuration
+     'db' => array(
+        'connection_string' => 'mysql:host=localhost;dbname=name',
+         'type' => 'mysql',
+         'user' => 'user',
+         'password' => 'pass',
+     ),
+
+    'base_url' => , // no trailing slash ie https://website.com/directory/serve
+    'site_title' => 'ChainTorrent Tracker',
+    'max_torrent_size' => 20*1024*1024,
+);
+
+ 
